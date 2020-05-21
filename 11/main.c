cb92f071c67f074a487739a50f222ace12377a26
Marta Grzesiak <mrtgrzesiak@gmail.com>
2020-05-21 13:07:59 +0200

    update main.c FFT

--- 11/main.c
+++ 11/main.c
@@ -34,25 +34,24 @@ int main() {
         for(int i = 1; i <= N; i++) {
             float ti = dt * (i - 1);
             f[2 * i - 1] = y[2 * i - 1] = sin((2 * M_PI / T) * ti) +  sin(2 * (2 * M_PI / T) * ti) + sin(3 * (2 * M_PI / T) * ti) + randDelta();
-			g1[2 * i - 1] = g2[2 * i - 1] = (1.0 / (sigma * sqrt(2 * M_PI))) * exp(-(ti * ti) / (2 * sigma * sigma));
-			f[2 * i] = y[2 * i] = g1[2 * i] = g2[2 * i] = 0;
-
-		}
-
-		four1(f, N, 1);
-		four1(g1, N, 1);
-		four1(g2, N, -1);
+	    g1[2 * i - 1] = g2[2 * i - 1] = (1.0 / (sigma * sqrt(2 * M_PI))) * exp(-(ti * ti) / (2 * sigma * sigma));
+	    f[2 * i] = y[2 * i] = g1[2 * i] = g2[2 * i] = 0;
+	}
+	    
+	four1(f, N, 1);
+	four1(g1, N, 1);
+	four1(g2, N, -1);
 
         for (int i = 1; i <= N; i++) {
-			float a1 = f[2 * i - 1];
-			float b1 = f[2 * i];
-			float a2 = g1[2 * i - 1] + g2[2 * i - 1];
-			float b2 = g1[2 * i] + g2[2 * i];
-			f[2 * i - 1] = a1 * a2 - b1 * b2;
-			f[2 * i] = a1 * b2 + a2 * b1;
-		}
+	    float a1 = f[2 * i - 1];
+	    float b1 = f[2 * i];
+	    float a2 = g1[2 * i - 1] + g2[2 * i - 1];
+	    float b2 = g1[2 * i] + g2[2 * i];
+	    f[2 * i - 1] = a1 * a2 - b1 * b2;
+	    f[2 * i] = a1 * b2 + a2 * b1;
+	}
 
-		four1(f, N, -1);
+	four1(f, N, -1);
         float max = fabs(f[1]);
 
         for(int i = 2; i <= N; i++) {
@@ -61,22 +60,22 @@ int main() {
 
         for(int j = 1; j <= N; j++) {
             float ti = dt * (j - 1);
-			fprintf(current, "%10f %10f\n", ti, y[2 * j - 1]);
+	    fprintf(current, "%10f %10f\n", ti, y[2 * j - 1]);
         }
 
         fprintf(current, "\n\n");
 
         for (int j = 1; j <= N; j++) {
-			float ti = dt * (j - 1);
+	    float ti = dt * (j - 1);
             fprintf(current, "%10f %10f\n", ti, f[2 * j - 1] * 2.5 / max);
         }
 
         free_vector(f, 1, 2 * N);
-		free_vector(y, 1, 2 * N);
-		free_vector(g1, 1, 2 * N);
-		free_vector(g2, 1, 2 * N);
+	free_vector(y, 1, 2 * N);
+	free_vector(g1, 1, 2 * N);
+	free_vector(g2, 1, 2 * N);
         fclose(current);
     }
-
+	
     return 0;
 }
